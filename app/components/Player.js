a245a85261f5b002bbfaa38ea725fe549b490913
Sabbir Ahmed <sabbiirr@gmail.com>
2016-12-30 10:37:14 +0600

    refactor

--- app/components/Player.js
+++ app/components/Player.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import wcjs from 'wcjs-prebuilt';
-import renderer from 'wcjs-renderer';
 import Controls from './Controls';
 import styles from './Player.css';
+import SPlayer from '../utils/SPlayer';
+
+const player = new SPlayer({ debug: true });
 
 class Player extends Component {
   constructor() {
     super();
-    this.vlc = wcjs.createPlayer();
     this.state = { progress: 0 };
   }
 
   componentDidMount() {
     document.addEventListener('dragover', e => e.preventDefault());
     document.addEventListener('drop', this.onDrop);
-    this.showPlayer();
+    player.renderVideo(this.playerCanvas);
+    player.addEventListener('onPositionChanged', (position) => {
+      this.setState({
+        progress: (position * 100)
+      });
+    });
   }
 
   componentWillUnmount() {
@@ -23,28 +28,14 @@ class Player extends Component {
   }
 
   onDrop = (e) => {
-    this.vlc.play(`file://${e.dataTransfer.files[0].path}`);
     e.preventDefault();
+    player.play(`file://${e.dataTransfer.files[0].path}`);
   };
-
-  initListeners() {
-    this.vlc.onPositionChanged = (value) => {
-      this.setState({
-        progress: value * 100
-      });
-    };
-  }
-
-  showPlayer() {
-    renderer.bind(this.playerCanvas, this.vlc, {});
-    this.initListeners();
-    window.vlc = this.vlc;
-  }
 
   render() {
     return (
       <div>
-        <Controls progress={this.state.progress} togglePause={this.vlc.togglePause}/>
+        <Controls progress={this.state.progress} togglePause={player.togglePause}/>
         <canvas
           className={styles.player}
           ref={(playerCanvas) => { this.playerCanvas = playerCanvas; }}
---
+++ app/utils/supportedEvents.js
@@ -0,0 +1,23 @@
+const supportedEvents = [
+  'onBackward',
+  'onBuffering',
+  'onEncounteredError',
+  'onEndReached',
+  'onForward',
+  'onLengthChanged',
+  'onLogMessage',
+  'onMediaChanged',
+  'onNothingSpecial',
+  'onOpening',
+  'onPausableChanged',
+  'onPaused',
+  'onPlaying',
+  'onPositionChanged',
+  'onSeekableChanged',
+  'onStopped',
+  'onTimeChanged',
+  'onFrameCleanup',
+  'onFrameReady',
+  'onFrameSetup'
+];
+export default supportedEvents;
